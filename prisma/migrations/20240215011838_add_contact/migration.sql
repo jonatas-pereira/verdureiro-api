8ba030ebac74043a2c966e3b389658d7e62cea54
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-02-25 12:28:10 -0300

    Delete migrations

--- prisma/migrations/20240215011838_add_contact/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `contact` to the `reservations` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "reservations" ADD COLUMN     "contact" TEXT NOT NULL;
